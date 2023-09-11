6c5b4e4663acb4d3991cf936f5ade964ba861c3d
alejo78912 <av739006@gmail.com>
2023-09-11 17:05:33 -0500

    nuevos cambios

--- src/app/producto.model.ts
+++ src/app/producto.model.ts
@@ -5,16 +5,16 @@ export class Producto {
     descripcionProducto: string;
     nombreProducto: string;
     valor: number;
-    urlPhoto : string;
+    url_photo : string;
     nombreCategoria: string;
 
-    constructor(codProducto: number,cantidadAVender: string, nombreProducto: string, descripcionProducto: string, valor: number, urlPhoto: string, nombreCategoria:string) {
+    constructor(codProducto: number,cantidadAVender: string, nombreProducto: string, descripcionProducto: string, valor: number, url_photo: string, nombreCategoria:string) {
         this.codProducto = codProducto;
         this.cantidadAVender = cantidadAVender;
         this.nombreProducto = nombreProducto;
         this.descripcionProducto = descripcionProducto;
         this.valor = valor;
-        this.urlPhoto = urlPhoto;
+        this.url_photo = url_photo;
         this.nombreCategoria = nombreCategoria;
     }
 }
