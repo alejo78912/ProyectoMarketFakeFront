a98f8ecbc84662d7372c394f53b880547bb6c07e
Juan Acosta <jpabloacosta@192.168.1.10>
2023-10-08 14:15:12 -0500

    Busquedas

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -13,6 +13,7 @@ import { SharedServiceService } from '../shared-service.service';
 })
 export class ItemsComponent implements OnInit{
   productos: Product[] = [];
+  auxProducts:Product[] = this.productos;
   filteredProducts:Product[] = [];
   cart: Product[] = [];
   busque: string = '';
@@ -25,21 +26,27 @@ export class ItemsComponent implements OnInit{
 
 
   capture(busqued: any) {
-    this.busque = busqued;
+    
     console.log(this.busque);
     this.filterProducts();
     console.log(this.filteredProducts);
     if(this.busque!=''){
-      this.productos=this.filteredProducts
+      this.auxProducts=this.filteredProducts
+    }else{
+      this.auxProducts=this.productos
+      
     }
     
     // You can perform additional actions here based on the search query.
   }
 
   filterProducts(){
+    
     this.filteredProducts=[];
     for (let i = 0; i < this.productos.length; i++) {
-      if(this.productos[i].productName.toLowerCase().includes(this.busque.toLowerCase())){
+      if(this.productos[i].productName.toLowerCase().includes(this.busque.toLowerCase()) || 
+      this.productos[i].productDescription.toLowerCase().includes(this.busque.toLowerCase()) ||
+      this.productos[i].category.categoryName.toLowerCase().includes(this.busque.toLowerCase()) ){
         this.filteredProducts.push(this.productos[i]);
       }
     }
@@ -69,6 +76,7 @@ export class ItemsComponent implements OnInit{
   ngOnInit(): void {
     this.ProductsService.productos().subscribe(data => {
       this.productos = data;
+      this.auxProducts=this.productos
     });
 
     this.sharedService.getSearchQuery().subscribe((query) => {
